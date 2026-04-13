e3fb6236619fe757bf21bec302eaf1874112707d
AsadIndustries <xudoyberganovasadbek60@gmail.com>
2026-04-13 08:52:55 +0500

    fix: handle zero edge case

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ def gcd(a, b):
     return a
 
 def lcm(a, b):
+    if a == 0 or b == 0:
+        return 0
     return a * b // gcd(a, b)
 
 @app.route('/xudoyberganovasadbek60_gmail_com')
@@ -15,7 +17,7 @@ def compute():
     x = request.args.get('x')
     y = request.args.get('y')
 
-    if not x or not y:
+    if x is None or y is None:
         return "NaN"
 
     if not x.isdigit() or not y.isdigit():
@@ -25,6 +27,3 @@ def compute():
     y = int(y)
 
     return str(lcm(x, y))
-
-if __name__ == '__main__':
-    app.run()
